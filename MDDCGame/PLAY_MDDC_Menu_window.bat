241caad817556ed4e4345742e2c1586c47d1e120
Filip Bártek <Filip.Bartek@hotmail.com>
2019-01-21 17:57:01 +0100

    Restrict window resolution to 1024x768
    This is the only resolution supported by the game.

--- MDDCGame/PLAY_MDDC_Menu_window.bat
+++ MDDCGame/PLAY_MDDC_Menu_window.bat
@@ -1,5 +1,5 @@
 setlocal
 call global.bat
 call local.bat
-%udk% MainMenu-new?game=MDDCGame.MDDCGame %ini% -LOG %*
+%udk% MainMenu-new?game=MDDCGame.MDDCGame %ini% -LOG -resx=1024 -resy=768 %*
 endlocal
