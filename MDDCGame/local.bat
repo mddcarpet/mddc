c56c2f1b0b5d5becce723772ded0784ccde93ffe
Filip Bártek <Filip.Bartek@hotmail.com>
2019-01-23 13:56:35 +0100

    Make the generated config filenames consistent with UDKGame

--- MDDCGame/local.bat
+++ MDDCGame/local.bat
@@ -8,8 +8,8 @@ rem Non-DEF inis are redirected so that the default non-DEF inis needn't be over
 rem https://api.unrealengine.com/udk/Three/CommandLineArguments.html#INI/Config%20Files
 set ini=^
 -DEFENGINEINI=%configpath%\DefaultEngine.ini ^
--ENGINEINI=%configpath%\Engine.ini ^
+-ENGINEINI=%configpath%\MDDCEngine.ini ^
 -DEFGAMEINI=%configpath%\DefaultGame.ini ^
--GAMEINI=%configpath%\Game.ini ^
+-GAMEINI=%configpath%\MDDCGame.ini ^
 -DEFINPUTINI=%configpath%\DefaultInput.ini ^
--INPUTINI=%configpath%\Input.ini
+-INPUTINI=%configpath%\MDDCInput.ini
