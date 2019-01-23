51435d209449d84cc11ddb8ac3fbdfed453af2e3
Filip Bártek <Filip.Bartek@hotmail.com>
2019-01-23 13:39:25 +0100

    Remove an unnecessary documentation comment (trivial)

--- MDDCGame/local.bat
+++ MDDCGame/local.bat
@@ -4,5 +4,4 @@ set gamename=MDDC
 
 rem DEF* inis are redirected so that we can use our own configuration without altering the default UDK files.
 rem Non-DEF inis are redirected so that the default non-DEF inis needn't be overwritten. (FB: At least I think so.)
-rem Paths are relative to udk.exe. (FB: Probably.)
 set ini=-DEFENGINEINI=..\..\%gamename%Game\Config\DefaultEngine.ini -ENGINEINI=..\..\%gamename%Game\Config\Engine.ini -DEFGAMEINI=..\..\%gamename%Game\Config\DefaultGame.ini -GAMEINI=..\..\%gamename%Game\Config\Game.ini -DEFINPUTINI=..\..\%gamename%Game\Config\DefaultInput.ini -INPUTINI=..\..\%gamename%Game\Config\Input.ini
