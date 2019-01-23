2e2b9a7c0862c5cd2059ea7a9bd3e9797d4fb43e
Filip Bártek <Filip.Bartek@hotmail.com>
2019-01-23 14:09:33 +0100

    Add more custom inis to write less into UDKGame/Config
    Two inis are still created in UDKGame/Config by udk-make:
    * UDKEditorUserSettings.ini (equivalent created in MDDCGame/Config too)
    * UDKSystemSettings.ini (I can't see a way to override its path)

--- MDDCGame/local.bat
+++ MDDCGame/local.bat
@@ -7,9 +7,13 @@ rem DEF* inis are redirected so that we can use our own configuration without al
 rem Non-DEF inis are redirected so that the default non-DEF inis needn't be overwritten. (FB: At least I think so.)
 rem https://api.unrealengine.com/udk/Three/CommandLineArguments.html#INI/Config%20Files
 set ini=^
+-EDITORINI=%configpath%\MDDCEditor.ini ^
+-EDITORUSERSETTINGSINI=%configpath%\MDDCEditorUserSettings.ini ^
+-LIGHTMASSINI=%configpath%\MDDCLightmass.ini ^
 -ENGINEINI=%configpath%\MDDCEngine.ini ^
 -GAMEINI=%configpath%\MDDCGame.ini ^
 -INPUTINI=%configpath%\MDDCInput.ini ^
+-UIINI=%configpath%\MDDCUI.ini ^
 -DEFENGINEINI=%configpath%\DefaultEngine.ini ^
 -DEFGAMEINI=%configpath%\DefaultGame.ini ^
 -DEFINPUTINI=%configpath%\DefaultInput.ini
