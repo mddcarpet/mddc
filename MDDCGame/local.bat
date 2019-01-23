0b9e315ad3ba7335b59529a54eecf08db1790ff7
Filip Bártek <Filip.Bartek@hotmail.com>
2019-01-23 14:07:59 +0100

    Reorder ini file paths so that custom precede default

--- MDDCGame/local.bat
+++ MDDCGame/local.bat
@@ -7,9 +7,9 @@ rem DEF* inis are redirected so that we can use our own configuration without al
 rem Non-DEF inis are redirected so that the default non-DEF inis needn't be overwritten. (FB: At least I think so.)
 rem https://api.unrealengine.com/udk/Three/CommandLineArguments.html#INI/Config%20Files
 set ini=^
--DEFENGINEINI=%configpath%\DefaultEngine.ini ^
 -ENGINEINI=%configpath%\MDDCEngine.ini ^
--DEFGAMEINI=%configpath%\DefaultGame.ini ^
 -GAMEINI=%configpath%\MDDCGame.ini ^
--DEFINPUTINI=%configpath%\DefaultInput.ini ^
--INPUTINI=%configpath%\MDDCInput.ini
+-INPUTINI=%configpath%\MDDCInput.ini ^
+-DEFENGINEINI=%configpath%\DefaultEngine.ini ^
+-DEFGAMEINI=%configpath%\DefaultGame.ini ^
+-DEFINPUTINI=%configpath%\DefaultInput.ini
