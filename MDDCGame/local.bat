aa450d38d953816c0d571773fa9a966c5ad26578
Filip Bártek <Filip.Bartek@hotmail.com>
2019-01-23 13:53:13 +0100

    Simplify setting the config file paths

--- MDDCGame/local.bat
+++ MDDCGame/local.bat
@@ -1,14 +1,15 @@
 rem Local configuration. Should be configured separately for each PC. MDDC-specific.
 
 set gamename=MDDC
+set configpath=..\..\%gamename%Game\Config
 
 rem DEF* inis are redirected so that we can use our own configuration without altering the default UDK files.
 rem Non-DEF inis are redirected so that the default non-DEF inis needn't be overwritten. (FB: At least I think so.)
 rem https://api.unrealengine.com/udk/Three/CommandLineArguments.html#INI/Config%20Files
 set ini=^
--DEFENGINEINI=..\..\%gamename%Game\Config\DefaultEngine.ini ^
--ENGINEINI=..\..\%gamename%Game\Config\Engine.ini ^
--DEFGAMEINI=..\..\%gamename%Game\Config\DefaultGame.ini ^
--GAMEINI=..\..\%gamename%Game\Config\Game.ini ^
--DEFINPUTINI=..\..\%gamename%Game\Config\DefaultInput.ini ^
--INPUTINI=..\..\%gamename%Game\Config\Input.ini
+-DEFENGINEINI=%configpath%\DefaultEngine.ini ^
+-ENGINEINI=%configpath%\Engine.ini ^
+-DEFGAMEINI=%configpath%\DefaultGame.ini ^
+-GAMEINI=%configpath%\Game.ini ^
+-DEFINPUTINI=%configpath%\DefaultInput.ini ^
+-INPUTINI=%configpath%\Input.ini
