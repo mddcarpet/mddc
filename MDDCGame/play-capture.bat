bebdd82f62798622593887215ce33abebeda9ae9
Filip Bártek <Filip.Bartek@hotmail.com>
2019-01-24 14:33:39 +0100

    Capture gameplay videos fullscreen to ensure full resolution

--- MDDCGame/play-capture.bat
+++ MDDCGame/play-capture.bat
@@ -1,2 +1,3 @@
+rem We run the game fullscreen to ensure the correct resolution.
 rem https://api.unrealengine.com/udk/Three/CapturingCinematicsAndGameplay.html
-play -BENCHMARK -FPS=25 -DUMPMOVIE -MAXQUALITYMODE %*
+play-fullscreen -BENCHMARK -FPS=25 -DUMPMOVIE -MAXQUALITYMODE %*
